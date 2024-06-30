797c61e8c21a69d57fb3a593e688b7b8030a44c6
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-06-30 16:53:19 +0600

    reset program

---
+++ src/reset.jsx
@@ -0,0 +1,93 @@
+// import React from 'react';
+
+// const reset = () => {
+//     return (
+//         <div>
+            
+//   "code-runner.executorMap": {
+//     "javascript": "node",
+//     "java": "cd $dir && javac $fileName && java $fileNameWithoutExt",
+//     "c": "cd $dir && gcc $fileName -o $fileNameWithoutExt && $dir$fileNameWithoutExt",
+//     "zig": "zig run",
+//     "cpp": "cd $dir && g++ $fileName -o $fileNameWithoutExt && $dir$fileNameWithoutExt",
+//     "objective-c": "cd $dir && gcc -framework Cocoa $fileName -o $fileNameWithoutExt && $dir$fileNameWithoutExt",
+//     "php": "php",
+//     "python": "python -u",
+//     "perl": "perl",
+//     "perl6": "perl6",
+//     "ruby": "ruby",
+//     "go": "go run",
+//     "lua": "lua",
+//     "groovy": "groovy",
+//     "powershell": "powershell -ExecutionPolicy ByPass -File",
+//     "bat": "cmd /c",
+//     "shellscript": "bash",
+//     "fsharp": "fsi",
+//     "csharp": "scriptcs",
+//     "vbscript": "cscript //Nologo",
+//     "typescript": "ts-node",
+//     "coffeescript": "coffee",
+//     "scala": "scala",
+//     "swift": "swift",
+//     "julia": "julia",
+//     "crystal": "crystal",
+//     "ocaml": "ocaml",
+//     "r": "Rscript",
+//     "applescript": "osascript",
+//     "clojure": "lein exec",
+//     "haxe": "haxe --cwd $dirWithoutTrailingSlash --run $fileNameWithoutExt",
+//     "rust": "cd $dir && rustc $fileName && $dir$fileNameWithoutExt",
+//     "racket": "racket",
+//     "scheme": "csi -script",
+//     "ahk": "autohotkey",
+//     "autoit": "autoit3",
+//     "dart": "dart",
+//     "pascal": "cd $dir && fpc $fileName && $dir$fileNameWithoutExt",
+//     "d": "cd $dir && dmd $fileName && $dir$fileNameWithoutExt",
+//     "haskell": "runghc",
+//     "nim": "nim compile --verbosity:0 --hints:off --run",
+//     "lisp": "sbcl --script",
+//     "kit": "kitc --run",
+//     "v": "v run",
+//     "sass": "sass --style expanded",
+//     "scss": "scss --style expanded",
+//     "less": "cd $dir && lessc $fileName $fileNameWithoutExt.css",
+//     "FortranFreeForm": "cd $dir && gfortran $fileName -o $fileNameWithoutExt && $dir$fileNameWithoutExt",
+//     "fortran-modern": "cd $dir && gfortran $fileName -o $fileNameWithoutExt && $dir$fileNameWithoutExt",
+//     "fortran_fixed-form": "cd $dir && gfortran $fileName -o $fileNameWithoutExt && $dir$fileNameWithoutExt",
+//     "fortran": "cd $dir && gfortran $fileName -o $fileNameWithoutExt && $dir$fileNameWithoutExt",
+//     "sml": "cd $dir && sml $fileName"
+//   },
+//   "code-runner.clearPreviousOutput": true,
+//   "code-runner.saveFileBeforeRun": true,
+//   "liveServer.settings.root": "",
+//   "code-runner.executorMap": {
+//     "php": "c:\\php\\php.exe"
+//   },
+//   "editor.defaultFormatter": "esbenp.prettier-vscode",
+//   "editor.formatOnSave": true,
+//   "editor.codeActionsOnSave": {
+//     "source.fixAll.eslint": "explicit",
+//     "source.fixAll.tslint": "explicit",
+//     "source.organizeImports": "explicit"
+//   },
+//   "eslint.run": "onSave",
+//   "emmet.includeLanguages": {
+//     "javascript": "javascriptreact"
+//   },
+//   "path-autocomplete.extensionOnImport": true,
+//   "path-autocomplete.excludedItems": {
+//     "*/.js": {
+//       "when": "**"
+//     },
+//     "*/.jsx": {
+//       "when": "**"
+//     }
+//   },
+//   "javascript.validate.enable": false,
+//   "typescript.validate.enable": false
+//         </div>
+//     );
+// };
+
+// export default reset;
